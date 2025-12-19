f6581d3a868605e624bd341b0323e92815dadb42
KaviAR1488 <goldscaer1349@gmail.com>
2025-12-19 13:14:24 +0500

    Add files via upload

---
+++ Оаип/3 дедлайн/Контрольная работа/5 вариант.py
@@ -0,0 +1,16 @@
+from dataclasses import dataclass
+@dataclass
+class Student:
+    name:str
+    age:int
+    average_grade:float
+students=[
+    Student("Анна",20,4.5),
+    Student("Иван",21,3.8),
+    Student("Мария",19,4.9),
+    Student("Петр",22,4.2),
+    Student("Ольга",20,3.5)
+]
+students_sorted=sorted(students,key=lambda x:x.average_grade,reverse=True)
+for student in students_sorted:
+    print(f"{student.name}:{student.average_grade}")
